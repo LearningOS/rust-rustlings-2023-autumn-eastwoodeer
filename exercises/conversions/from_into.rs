bb58f47d74e00e3bb5fe477811c41636b26c3612
Chen Bin <chen.bin11@zte.com.cn>
2023-10-13 11:11:10 +0800

    conversions done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,18 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let info: Vec<&str> = s.split(",").collect();
+        if info.len() == 2 && info[0].len() != 0 {
+            if let Ok(age) = info[1].parse::<usize>() {
+                return Person {
+                    name: String::from(info[0]),
+                    age: age,
+                };
+            }
+        }
+        Default::default()
     }
 }
 
